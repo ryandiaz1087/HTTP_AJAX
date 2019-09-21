cd6578ffeb18b2f17ea3019f8b43aa6f6541b8f1
ryananthonydiaz <email@ryananthonydiaz.com>
2019-09-21 15:39:05 -0700

    React Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,47 +1,11 @@
 import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from '../../../src/axios';
+import { Route } from 'react-router-dom';
+import Posts from '../Blog/Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-	state = {
-		posts: [],
-		selectedId: null,
-	};
-
-	componentDidMount() {
-		axios.get('/posts').then(response => {
-			const posts = response.data.slice(0, 4);
-			const updatedPosts = posts.map(post => {
-				return {
-					...post,
-					author: 'Max',
-				};
-			});
-			this.setState({ posts: updatedPosts });
-			// console.log(response);
-		});
-	}
-
-	postSelectedHandler = id => {
-		this.setState({ selectedId: id });
-	};
-
 	render() {
-		const posts = this.state.posts.map(post => {
-			return (
-				<Post
-					key={post.id}
-					title={post.title}
-					author={post.author}
-					clicked={() => this.postSelectedHandler(post.id)}
-				/>
-			);
-		});
-
 		return (
 			<div className="Blog">
 				<header>
@@ -50,11 +14,12 @@ class Blog extends Component {
 							<a href="/">HOME</a>
 						</li>
 						<li>
-							<a href="/">New Post</a>
+							<a href="/new-post">New Post</a>
 						</li>
 					</ul>
 				</header>
-				<section className="Posts">{posts}</section>
+				<Route path="/" exact component={Posts} />
+				<Route path="/new-post" component={NewPost} />
 			</div>
 		);
 	}
